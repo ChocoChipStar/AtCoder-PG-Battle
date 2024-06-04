231670ccca9580477279e7a6e2cf33fb88cac965
齊藤元紀 <saitogenki20031104@gmail.com>
2024-06-04 14:59:09 +0900

    FizzBuzz問題　完成

--- AtCoder-PG_Battle/AtCoder-PG_Battle/main.cpp
+++ AtCoder-PG_Battle/AtCoder-PG_Battle/main.cpp
@@ -1,10 +1,25 @@
 #include<iostream>
-
+#define VAR_NAME(var) #var
 using namespace std;
 
 int main(void)
 {
+	constexpr int FIZZ = 3;
+	constexpr int BUZZ = 5;
+	
+	constexpr int LOOP_START_VAL = 1;
+	constexpr int LOOP_COUNT = 100;
 
+	string fizz[]{ "",VAR_NAME(FIZZ) };
+	string buzz[]{ "",VAR_NAME(BUZZ) };
+
+	for (int i = LOOP_START_VAL; i < LOOP_COUNT; ++i)
+	{
+		cout << fizz[i % 3 == 0];
+		cout << buzz[i % 5 == 0];
+
+		cout << i << endl;
+	}
 
 	return 0;
 }
