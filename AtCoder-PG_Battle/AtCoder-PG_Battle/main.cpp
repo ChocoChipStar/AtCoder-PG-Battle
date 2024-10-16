f6f1864f9e4d5f2d161508ccecd2c2316e0ca8d2
齊藤元紀 <saitogenki20031104@gmail.com>
2024-10-16 16:41:56 +0900

    [Add]

--- AtCoder-PG_Battle/AtCoder-PG_Battle/main.cpp
+++ AtCoder-PG_Battle/AtCoder-PG_Battle/main.cpp
@@ -2,9 +2,43 @@
 
 using namespace std;
 
+int gcd(int a, int b)
+{
+	if (a % b == 0) 
+	{
+		return b;
+	}
+	else 
+	{
+		return gcd(b, a % b);
+	}
+}
+
 int main(void)
 {
+	// ‘•”‰®‚Ì”
+	int n = 0;
 
+	// ‰½•”‰®’u‚«‚É‘|œ‚·‚é‚©
+	int m = 0;
+
+	cin >> n;
+	cin >> m;
+
+	int answer = 0;
+	
+	answer = gcd(n, m);
+
+	if (answer == 1)
+	{
+		cout << "yes" << endl;
+	}
+	else
+	{
+		cout << "no" << endl;
+	}
 
 	return 0;
+	// 71400
+	// 38896
 }
