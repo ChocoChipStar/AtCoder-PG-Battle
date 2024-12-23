84defc7af6a09a895c06597c46778ca22569802a
齊藤元紀 <saitogenki20031104@gmail.com>
2024-12-23 09:14:41 +0900

    [Add]

--- AtCoder-PG_Battle/AtCoder-PG_Battle/main.cpp
+++ AtCoder-PG_Battle/AtCoder-PG_Battle/main.cpp
@@ -4,7 +4,40 @@ using namespace std;
 
 int main(void)
 {
+	string abcd;
 
+	cin >> abcd;
 
+	string plusAndMinus[]{ "+", "-"};
+	
+	int number[4]{};
+	const int A = 0;
+	const int B = 1;
+	const int C = 2;
+	const int D = 3;
+	
+	for (int i = 0; i < 4; i++)
+	{
+		number[i] = abcd[i] - '0';
+	}
+
+	int total = 0;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			for (int k = 0; k < 2; k++)
+			{
+				if(i == )
+				total = number[A]plusAndMinus[i],number[B],plusAndMinus[j],number[C],plusAndMinus[k],number[D];
+				if (total == 7)
+				{
+					//cout << number[A], plusAndMinus[i], number[B], plusAndMinus[j], number[C], plusAndMinus[k], number[D] << endl;
+					break;
+				}
+			}
+		}
+	}
+	
 	return 0;
 }
